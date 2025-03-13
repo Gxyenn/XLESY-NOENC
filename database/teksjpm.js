ceb47e828d68445a0b18b3d36870c98caef5cf05
Gxyenn <gxyennxlesy@gmail.com>
2025-03-13 09:50:45 +0700

    Update teksjpm.js

--- database/teksjpm.js
+++ database/teksjpm.js
@@ -1,12 +1,3 @@
-*XlesyVIP*
-*Creator Gxyenn*
-`Price harga Script XlesyVIP`
-_15K ENC_
-_30K NO ENC_
-> Kelebihan Script
-_Bisa Run di Termux/Panel_.
-_Fitur Lumayan Ada 527_.
-_Dan Lainya_
-
-*_Silahkan Pm JIKALAU Minat_*
-_owner_ : https://wa.me/623877636168?text=Order_Script_Xlesy-VIP
+XlesyVIP
+KALO MAU BELI SCRIPT NO ENC 
+  PM https://wa.me/6283877636168
